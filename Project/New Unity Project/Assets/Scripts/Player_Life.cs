8de7f1cb97750d777c66bb70f9e5143206bcfd0a
Ayslan Vieira Fontes <78982648+AyslanSE@users.noreply.github.com>
2021-04-06 02:23:51 -0300

    life

--- Project/New Unity Project/Assets/Scripts/Player_Life.cs
+++ Project/New Unity Project/Assets/Scripts/Player_Life.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Life : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Slider LifeBar;
+    [SerializeField] private int life = 50;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LifeBar.value = life;
     }
 }
