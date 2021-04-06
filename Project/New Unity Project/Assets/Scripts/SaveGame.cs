b73d0479c48ec4855fbb27e7dd172608358b69ca
Ayslan Vieira Fontes <78982648+AyslanSE@users.noreply.github.com>
2021-04-06 03:37:58 -0300

    finalizado

--- Project/New Unity Project/Assets/Scripts/SaveGame.cs
+++ Project/New Unity Project/Assets/Scripts/SaveGame.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
 
-public class SaveGame : MonoBehaviour
+[XmlRoot("dados")] public class Dados
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float posx;
+    public float posy;
 
-    // Update is called once per frame
-    void Update()
+    [XmlElement("PosX", typeof(float))]
+    public float PosX
     {
-        
+        get { return this.posx; }
+        set { this.posx = value; }
+    }
+    [XmlElement("PosY", typeof(float))]
+    public float PosY
+    {
+        get { return this.posy; }
+        set { this.posy = value; }
+    }
+}
+[SerializeField] public class SaveGame : MonoBehaviour
+{
+    public GameObject playerposition;
+    private string save;
+
+    private void Start()
+    {
+        save = Application.persistentDataPath + "/save1.dat";
+        Load();
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+            Save();
+        if (Input.GetKey(KeyCode.L))
+            Load();
+    }
+    void Save()
+    {
+        if (File.Exists(save))
+            File.Delete(save);
+        else
+            File.Create(save);
+
+        XmlSerializer x = new XmlSerializer(typeof(Dados));
+        StreamWriter writer = new StreamWriter(save, true);
+
+        Dados d = new Dados();
+        d.posx = playerposition.transform.position.x;
+        d.posy = playerposition.transform.position.y;
+
+        x.Serialize(writer, d);
+
+        writer.Close();
+    }
+    void Load()
+    {
+        XmlSerializer x = new XmlSerializer(typeof(Dados));
+        StreamReader reader = new StreamReader(save);
+
+        Dados d = (Dados)x.Deserialize(reader);
+        reader.Close();
+
+        playerposition.transform.position = new Vector2(d.PosX, d.PosY);
     }
 }
